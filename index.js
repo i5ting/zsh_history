a28dac5cfa1aac02248b9eefd7a5f4d8a2e8d475
alfred sang <i5ting@126.com>
2015-05-28 08:37:46 +0800

    bump to v1.0.1

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ var current_path = process.cwd();
 child_process.execFile(file_path + '/process.sh',null,{cwd:current_path},function (error,stdout,stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
+    }else{
+      console.log('fix zsh corrupt history issue success');
     }
 });
